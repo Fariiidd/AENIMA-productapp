c5a354aa961e07c5cc6d237f72a3cd9276521fe1
Farid <faridurielrodriguez@gmail.com>
2022-05-17 12:29:31 -0300

    Config server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+import dotenv from 'dotenv';
+dotenv.config();
+import app from './app.js';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(3000, () => console.log(`Listening at port ${PORT}`));
