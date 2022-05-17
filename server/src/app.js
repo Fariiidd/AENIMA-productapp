092b121f13767e6667345831d9afd1f9fb163565
Farid <faridurielrodriguez@gmail.com>
2022-05-17 13:42:03 -0300

    Create controllers

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import connectDB from './config/db.js';
-
+import bodyParser from 'body-parser';
+import cors from 'cors'
 
 //ROUTE
 import productRoute from './routes/product-route.js';
 
 connectDB()
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 app.use('/api/product', productRoute);
 
--- server/src/controller/productController.js
+++ server/src/controller/productController.js
@@ -1,19 +1,68 @@
-export const getProducts = (req, res) => {
-    res.send("Getting products");
+import Product from "../models/Product.js";
+import mongoose from "mongoose";
+
+export const getProducts = async (req, res) => {
+    try {
+        const result = await Product.find();
+
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-export const createProduct = (req, res) => {
-    res.send("Create product");
+export const createProduct = async (req, res) => {
+    const { name, description, image } = req.body;
+
+    const newProduct = new Product({ name, description, image });
+    try {
+        await newProduct.save();
+
+        res.status(200).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-export const getOneProduct = (req, res) => {
-    res.send("Getting one product");
+export const getOneProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await Product.findById(id);
+
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-export const updateProduct = (req, res) => {
-    res.send("Updating");
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const { name, description, image } = req.body;
+
+    const updateProduct = { name, description, image, _id: id }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    try {
+        await Product.findByIdAndUpdate(id, updateProduct, { new: true });
+
+        res.status(200).json(updateProduct)
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-export const deleteProduct = (req, res) => {
-    res.send("Deleting");
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+        await Product.findByIdAndDelete(id)
+
+        res.status(201).json({message: "Post deleted"})
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
